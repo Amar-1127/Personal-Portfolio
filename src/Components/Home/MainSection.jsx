0793ee9ff18f5efbfdd50bd19af08e332eb10550
Amar-1127 <amarsinha1127@gmail.com>
2025-06-17 19:36:45 -0700

    Adding MainScetion with photo

---
+++ src/Components/Home/MainSection.jsx
@@ -0,0 +1,20 @@
+export default function MainSection() {
+    return  (
+        <section id="mainSection" className="MainSection">
+            <div className="mainSection-contentbox">
+                <div className="mainsection-content">
+                    <p className="section-title">Hello, I am
+                    </p>
+                    <h1 className="mainSection-title">
+                        <span className="mainSection-title-color">Amar Sinha</span> 
+                    </h1>
+                    <p className="mainSection-description"> Second-Year Computer Science Student <br /> I'm currently building my skills in programming and development. </p>
+                </div>
+                <button className="button-primary">Get in touch</button>
+            </div>
+            <div className="mainSection-img">
+                <img src="./img/Amar's.jpg" alt="Amar's Photo"/>
+            </div>
+        </section>
+    );
+}
