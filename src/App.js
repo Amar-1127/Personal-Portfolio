a0c4085644dd0e8f2bc0d4d9ac04a2e93c12fa04
Amar-1127 <amarsinha1127@gmail.com>
2025-06-18 13:34:16 -0700

    Added Navbar and its styling

--- src/App.js
+++ src/App.js
@@ -1,11 +1,23 @@
 import './App.css';
 import React from "react";
 import { BrowserRouter as Router, Route, Routes} from "react-router-dom";
+import Navbar from './Components/Home/NavBar';
 import Home from './Components/Home/HomeScreen/Index';
+import MainSection from './Components/Home/MainSection';
+
 function App() {
   return (
-    <div className="App"> 
-    <Home />
+    <div className="App">
+      <Router>
+        <div>
+          <Navbar />
+          <Routes>
+            <Route path="/" element={<Home />}></Route>
+            <Route path="/MainSection" element={<MainSection />}></Route>
+            {/* <Route path="*" element={<div>404 Not Found</div>}></Route> */}
+          </Routes>
+        </div>
+      </Router>
     </div>
   );
 }
